322ad1900d74586d4a51f018b8373bab8fcf994d
brunasantosveiga <brunasantos1978.bs@gmail.com>
2022-10-26 19:11:30 -0300

    Changing reducer values ​​through dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import React from "react";
+import { useDispatch } from "react-redux"; //hook para mudar os dados dos reducers
+import { setName, setAge } from "./redux/reducers/userReducer";
 import { useAppSelector } from "./redux/hooks/useAppSelector";
 
 function App() {
+  const dispatch = useDispatch();
   const user = useAppSelector((state) => state.user);
   //Para acessar o reducer que ta no store usando o useSelector personalizado do arquivo useAppSelector.ts
+
+  const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setName(e.target.value)); //passo a minha action para o dispatch
+  };
+  const handleAgeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setAge(parseInt(e.target.value)));
+  };
 
   return (
     <div>
@@ -10,7 +21,8 @@ function App() {
       <br />
       Tema: ...
       <hr />
-      <input type="text" value={user.name} />
+      <input type="text" value={user.name} onChange={handleNameInput} />
+      <input type="text" value={user.age} onChange={handleAgeInput} />
       <hr />
       <button>Mudar Tema</button>
     </div>
